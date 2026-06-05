0dddede34e51f439dd6ddc9aff457a2439a228ae
sousuke989898-dotcom <sousuke989898@gmail.com>
2026-06-05 12:26:58 +0900

    DirectedPointの追加
    ・DirectedPoint：マップ生成のために、ドアのTilePointに向き情報を持たせるように変更
    
    ・Room：Roomで端に存在するドアに向き情報を持たせるように変更

--- Assets/Scripts/Map/Room.cs
+++ Assets/Scripts/Map/Room.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 
 namespace Game.GridMap
 {
+    /// <summary>
+    /// 部屋の形状の二次元配列と、特殊なタイルを取得するクラス
+    /// </summary>
     public class Room
     {
         private RoomData _roomData;
         
         public TileType[,] Terrain {get; private set;}
-        public List<TilePoint> SpecialTiles {get; private set;}
+        public List<TilePoint> SpecialTiles {get; private set;} //扉、宝箱、罠などの、ギミックが存在するもの
 
         public Room(RoomData data)
         {
@@ -30,16 +33,33 @@ namespace Game.GridMap
                 for (int y = 0; y < h; y++)
                 {
                     Color color = texture.GetPixel(x, y);
-                    TileType currentTileType = ColorToTile(color);
-                    Terrain[x,y] = currentTileType;
+                    TileType tile = ColorToTile(color);
+                    Terrain[x,y] = tile;
 
                     Vector2Int localPos = new(x, y);
 
-                    if (currentTileType == TileType.UpStairs || 
-                        currentTileType == TileType.DownStairs || 
-                        currentTileType == TileType.Door)
+                    if (tile == TileType.Door)
                     {
-                        SpecialTiles.Add(new TilePoint(currentTileType, localPos));
+                        Direction dir = Direction.None;
+
+                        if (x == 0)          dir = Direction.Left;  //左端
+                        else if (x == w - 1) dir = Direction.Right; //右端
+                        else if (y == 0)     dir = Direction.Down;  //上端
+                        else if (y == h - 1) dir = Direction.Upper; //下端
+                        //部屋の角に存在するアクセスできないドアは考慮していないため注意
+
+                        if (dir == Direction.None)
+                        {
+                            SpecialTiles.Add(new TilePoint(tile, localPos));
+                        }
+                        else
+                        {
+                            SpecialTiles.Add(new DirectedPoint(tile, localPos, dir));
+                        }
+                    }
+                    else if (tile == TileType.UpStairs || tile == TileType.DownStairs)
+                    {
+                        SpecialTiles.Add(new TilePoint(tile, localPos));
                     }
 
                 }
@@ -66,18 +86,4 @@ namespace Game.GridMap
 
     }
 
-    /// <summary>
-    /// 特殊なタイルの位置を保持するクラス
-    /// </summary>
-    public struct TilePoint
-    {
-        public TileType type; //タイル情報
-        public Vector2Int localPos; //位置情報
-
-        public TilePoint(TileType type, Vector2Int pos)
-        {
-            this.type = type;
-            this.localPos = pos;
-        }
-    }
 }
